b5efe9508b7a4c199931f57c274efca7156f51c4
Honghao Zhang <zhh358@gmail.com>
2015-08-02 12:22:21 -0400

    Updated demo project

--- Ji/ViewController.swift
+++ Ji/ViewController.swift
@@ -13,7 +13,22 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		let googleIndexData = NSData(contentsOfURL: NSURL(string: "https://www.google.com")!)!
-		let jiGoogleIndexDoc = Ji(htmlData: googleIndexData)
+		// Init with data
+		let googleIndexData = NSData(contentsOfURL: NSURL(string: "http://www.google.com")!)!
+		let jiGoogleIndexDoc = Ji(htmlData: googleIndexData)!
+		
+		let nodes = jiGoogleIndexDoc.searchWithXPathQuery("//body")
+		println("tag name: \(nodes?.first?.name)")
+		
+		// Init with URL
+		let jiAppleSupportDoc = Ji(htmlURL: NSURL(string: "http://www.apple.com/support")!)
+		let titleNode = jiAppleSupportDoc?.searchWithXPathQuery("//head/title")?.first
+		println("title: \(titleNode?.content)")
+		
+		// Init with String
+		let xmlString = "<?xml version='1.0' encoding='UTF-8'?><note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>"
+		let xmlDoc = Ji(xmlString: xmlString)
+		let bodyNode = xmlDoc?.rootNode?.firstChildWithName("body")
+		println("body: \(bodyNode?.content)")
 	}
 }
