aa65b2322f63b02b8114011b7b96339c8e2a6fd0
Honghao Zhang <zhh358@gmail.com>
2015-08-05 23:46:57 -0400

    minor updates on sample code

--- Ji/ViewController.swift
+++ Ji/ViewController.swift
@@ -16,18 +16,28 @@ class ViewController: UIViewController {
 		// Init with data
 		let googleIndexData = NSData(contentsOfURL: NSURL(string: "http://www.google.com")!)
 		if let googleIndexData = googleIndexData {
-			let jiGoogleIndexDoc = Ji(htmlData: googleIndexData)!
+			let jiDoc = Ji(htmlData: googleIndexData)!
+			let htmlNode = jiDoc.rootNode!
+			println("html tagName: \(htmlNode.tagName)")
 			
-			let nodes = jiGoogleIndexDoc.xPath("//body")
-			println("tag name: \(nodes?.first?.name)")
+			let aNodes = jiDoc.xPath("//body//a")
+			if let firstANode = aNodes?.first {
+				println("first a node tagName: \(firstANode.name)")
+				let href = firstANode["href"]
+				println("first a node href: \(href)")
+			}
 		} else {
 			println("google.com is inaccessible")
 		}
+		
+		println()
 		
 		// Init with URL
 		let jiAppleSupportDoc = Ji(htmlURL: NSURL(string: "http://www.apple.com/support")!)
 		let titleNode = jiAppleSupportDoc?.xPath("//head/title")?.first
 		println("title: \(titleNode?.content)")
+		
+		println()
 		
 		// Init with String
 		let xmlString = "<?xml version='1.0' encoding='UTF-8'?><note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>"
