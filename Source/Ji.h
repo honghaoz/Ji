7f53e89c141c3e5e9a4c629d024909787653401e
Honghao Zhang <zhh358@gmail.com>
2015-08-09 23:23:33 -0400

    Added framework support files

---
+++ Source/Ji.h
@@ -0,0 +1,17 @@
+//
+//  Ji.h
+//  Ji
+//
+//  Created by Honghao Zhang on 2015-08-09.
+//  Copyright (c) 2015 Honghao Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ji.
+FOUNDATION_EXPORT double JiVersionNumber;
+
+//! Project version string for Ji.
+FOUNDATION_EXPORT const unsigned char JiVersionString[];
+
+
