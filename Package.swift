34c2162c3a9cd2b694dbd014d1584520372f39d5
Thiago Holanda <unnamedd@gmail.com>
2016-03-28 20:59:11 -0300

    renamed package name

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "LibXML2",
+    name: "XML",
     dependencies: [
         .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 2),
